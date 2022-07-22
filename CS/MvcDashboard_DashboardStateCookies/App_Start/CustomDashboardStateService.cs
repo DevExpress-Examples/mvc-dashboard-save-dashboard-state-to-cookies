d177f51a71c8cb3b466dea7ffd94f1a7fe0c812d
Natalia Kazakova <natalia.kazakova@devexpress.com>
2022-07-22 17:24:58 +0400

    update client script

--- CS/MvcDashboard_DashboardStateCookies/App_Start/CustomDashboardStateService.cs
+++ CS/MvcDashboard_DashboardStateCookies/App_Start/CustomDashboardStateService.cs
@@ -6,7 +6,7 @@ using System.Xml.Linq;
 namespace MvcDashboard_DashboardStateCookies {
     internal class CustomDashboardStateService : IDashboardStateService {
         public DashboardState GetState(string dashboardId, XDocument dashboard) {
-            HttpCookie cookie = HttpContext.Current.Request.Cookies["MVCxDashboardState"];
+            HttpCookie cookie = HttpContext.Current.Request.Cookies["dashboardState"];
             if (cookie != null) {
                 DashboardState dashboardState = new DashboardState();
                 dashboardState.LoadFromJson(HttpUtility.UrlDecode(cookie.Value));
